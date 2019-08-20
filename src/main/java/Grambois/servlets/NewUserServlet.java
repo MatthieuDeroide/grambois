cfb3606914301156e5457a3b9011a8b0a0a38ab2
matthieu <matthieu.deroide@gmail.com>
2019-08-20 17:42:56 +0200

    nouveau mdp

--- src/main/java/Grambois/servlets/NewUserServlet.java
+++ src/main/java/Grambois/servlets/NewUserServlet.java
@@ -4,12 +4,14 @@ import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/newuser")
 public class NewUserServlet extends GenericServlet{
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -20,6 +22,6 @@ public class NewUserServlet extends GenericServlet{
 
         WebContext context = new WebContext(request, response, request.getServletContext());
 
-        /*engine.process("nwusr", context, response.getWriter());*/
+        engine.process("nwusr", context, response.getWriter());
     }
 }
